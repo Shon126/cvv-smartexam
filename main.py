0a5e6b69a81638310d7971e62c733485f93e686e
Shon126 <shonshaiju593@gmail.com>
2025-08-02 21:22:14 +0530

    ✅ Restrict student to one submission only

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import json
 import random
+random.shuffle(question_key)
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(json.loads(st.secrets["firebase"].to_json()))
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),  # Important for multiline key
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["firebase"]["universe_domain"]
+    })
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://cvv-smartexam-v2-default-rtdb.asia-southeast1.firebasedatabase.app'
     })
